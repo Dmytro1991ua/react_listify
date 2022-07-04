05e259d52e0de8ae975349e6f79098f719072784
Dmytro Kurchenko <dmytro.kurchenko@sigma.software>
2022-07-04 16:36:01 +0300

    Configure Stylelint, adjust scripts in package.json

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,5 +1,4 @@
 import { ReactElement } from 'react';
-import './App.css';
 
 const App = (): ReactElement => {
   return (
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './app/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
