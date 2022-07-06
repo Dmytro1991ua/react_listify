338948e8775bc7aa3c3e6b5accb461f45d9602c8
Dmytro Kurchenko <dmytro.kurchenko@sigma.software>
2022-07-06 19:18:19 +0300

    Setup basic app routing

---
+++ client/src/app/app.enums.ts
@@ -0,0 +1,6 @@
+export enum AppRoutes {
+  ShoppingLists = '/shopping-lists',
+  ShoppingList = '/shopping-lists/:shoppingListId',
+  SignIn = '/auth/sign-in',
+  SignUp = '/auth/sign-up',
+}
---
+++ client/src/app/modules/shopping-lists/shopping-lists.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from 'react';
+
+const ShoppingLists = (): ReactElement => {
+  return <div>ShoppingLists</div>;
+};
+
+export default ShoppingLists;
