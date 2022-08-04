5e52cd3fbf9a3b86d34cf8692c866303b9d55f6c
Dmytro Kurchenko <dmytro.kurchenko.-nd@disney.com>
2022-08-04 21:28:50 +0300

    Make adjustments in auth store

--- client/src/app/modules/auth/auth.store.ts
+++ client/src/app/modules/auth/auth.store.ts
@@ -4,15 +4,15 @@ import { devtools } from 'zustand/middleware';
 interface AuthStoreState {
   user: CurrentUser | null;
   loadingStatus: LoadingStatus;
-  setUser: (payload: CurrentUser | null) => void;
-  setLoadingStatus: (payload: LoadingStatus) => void;
+  setUser: (user: CurrentUser | null) => void;
+  setLoadingStatus: (loadingStatus: LoadingStatus) => void;
 }
 
 export const useAuthStore = create<AuthStoreState>()(
   devtools((set) => ({
     user: null,
     loadingStatus: 'loading',
-    setUser: (payload) => set(() => ({ user: payload }), false, 'setUser'),
-    setLoadingStatus: (payload) => set(() => ({ loadingStatus: payload }), false, 'setLoadingSTatus'),
+    setUser: (payload) => set((state) => ({ ...state, user: payload }), false, 'setUser'),
+    setLoadingStatus: (payload) => set((state) => ({ ...state, loadingStatus: payload }), false, 'setLoadingStatus'),
   }))
 );
