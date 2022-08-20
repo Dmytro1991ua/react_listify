c8fc506b5d60fe5351a80f427c5476cac860ff26
Dmytro Kurchenko <dmytro.kurchenko.-nd@disney.com>
2022-08-20 18:18:19 +0300

    Add endpont to create a new shopping list details item, update shopping lists routes

--- server/routes/shoppingListRoutes.ts
+++ server/routes/shoppingListRoutes.ts
@@ -5,10 +5,13 @@ import {
   deleteShoppingList,
   getAvailableShoppingLists,
 } from "../controller/shoppingListsController";
+import { createShoppingListDetailsItem } from "../controller/shoppingListsDetailsController";
 
 import { checkAuth } from "../middleware/authMiddleware";
 
 const router = express.Router();
+
+router.post("/:id/product-item", checkAuth, createShoppingListDetailsItem);
 
 router.route("/").get(checkAuth, getAvailableShoppingLists).post(checkAuth, createShoppingList);
 router.route("/:id").delete(checkAuth, deleteShoppingList);
