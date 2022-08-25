6e98a82d38f9cd1e5b4bfa067bcfdac3742dab30
Dmytro Kurchenko <dmytro.kurchenko.-nd@disney.com>
2022-08-25 11:20:22 +0300

    Create an endpoint on the server to delete a specific shopping list item

--- server/routes/shoppingListRoutes.ts
+++ server/routes/shoppingListRoutes.ts
@@ -5,13 +5,17 @@ import {
   deleteShoppingList,
   getAvailableShoppingLists,
 } from "../controller/shoppingListsController";
-import { createShoppingListDetailsItem } from "../controller/shoppingListsDetailsController";
+import {
+  createShoppingListDetailsItem,
+  deleteShoppingListDetailsItem,
+} from "../controller/shoppingListsDetailsController";
 
 import { checkAuth } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 router.put("/:id/product-item", checkAuth, createShoppingListDetailsItem);
+router.delete("/:id/delete-product-item", checkAuth, deleteShoppingListDetailsItem);
 
 router.route("/").get(checkAuth, getAvailableShoppingLists).post(checkAuth, createShoppingList);
 router.route("/:id").delete(checkAuth, deleteShoppingList);
