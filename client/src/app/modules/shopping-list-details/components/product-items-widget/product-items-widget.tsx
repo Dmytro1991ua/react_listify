f05d1ecd1336fa2ed25bc9ef46e5a25a03515139
Dmytro Kurchenko <dmytro.kurchenko.-nd@disney.com>
2022-09-14 21:14:10 +0300

    Implement logic of To Buy and Purchased product price calculation

--- client/src/app/modules/shopping-list-details/components/product-items-widget/product-items-widget.tsx
+++ client/src/app/modules/shopping-list-details/components/product-items-widget/product-items-widget.tsx
@@ -2,33 +2,50 @@ import { Box, Typography } from '@mui/material';
 import { ReactElement } from 'react';
 
 import { Currencies } from '../../../../app.enums';
+import { ShoppingListItem } from '../../../../app.interfaces';
+import { calculateProductItemsByCheckedSate } from '../../../../utils';
+import { useAuthStore } from '../../../auth/auth.store';
 import { Widget, WidgetContentWrapper } from './product-items-widget.styled';
 
 interface ProductItemsWidgetProps {
   currency: Currencies;
+  shoppingListItems: ShoppingListItem[];
 }
 
-const ProductItemsWidget = ({ currency }: ProductItemsWidgetProps): ReactElement => {
-  function commonLayout(label: string): JSX.Element {
+const ProductItemsWidget = ({ currency, shoppingListItems }: ProductItemsWidgetProps): ReactElement => {
+  const user = useAuthStore((state) => state.user);
+
+  const calculatedToBuy = calculateProductItemsByCheckedSate(
+    shoppingListItems,
+    false,
+    user?.calculateByQuantity ?? false
+  );
+  const calculatedPurchased = calculateProductItemsByCheckedSate(
+    shoppingListItems,
+    true,
+    user?.calculateByQuantity ?? false
+  );
+
+  function commonLayout(label: string, price: number): JSX.Element {
     return (
       <Box sx={{ display: 'flex', alignItems: 'center' }}>
         <Typography sx={{ margin: 0 }} variant='h4'>
           {label}:&nbsp;
         </Typography>
         <Typography sx={{ margin: 0 }} variant='h4'>
-          {currency}
+          {price ?? 0} {currency}
         </Typography>
       </Box>
     );
   }
 
-  const toBuy = commonLayout('To Buy');
-  const purchased = commonLayout('Purchased');
+  const toBuyLayout = commonLayout('To Buy', calculatedToBuy);
+  const purchasedLayout = commonLayout('Purchased', calculatedPurchased);
 
   const widgetContent = (
     <WidgetContentWrapper>
-      {toBuy}
-      {purchased}
+      {toBuyLayout}
+      {purchasedLayout}
     </WidgetContentWrapper>
   );
 
