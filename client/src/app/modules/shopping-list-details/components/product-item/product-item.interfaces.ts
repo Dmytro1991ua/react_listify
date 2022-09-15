70c6b2cef442feddf5034a82b5bfed88e5397e77
Dmytro Kurchenko <dmytro.kurchenko.-nd@disney.com>
2022-09-15 20:50:25 +0300

    Add a checkbox to each product item card, implement item selection on the checkbox check/uncheck

---
+++ client/src/app/modules/shopping-list-details/components/product-item/product-item.interfaces.ts
@@ -0,0 +1,23 @@
+import { ShoppingListData, ShoppingListItem } from '../../../../app.interfaces';
+
+export interface ProductItemProps {
+  /**
+   * @param {Partial<ShoppingListData & ShoppingListItem>} Defines a specific shopping list or shopping list item
+   */
+  item: Partial<ShoppingListData & ShoppingListItem>;
+  /**
+   * @param {string} name The name of the item a specific shopping list or shopping list item
+   * @default Currencies.Dollar
+   * @example Currencies.Dollar, Currencies.Euro
+   */
+  currency: string;
+  /**
+   * @param {boolean} Defines if calculateByQuantity property true of false in specific user (allows to calculate a product item price and total price of specific shopping list item)
+   * @default false
+   * @example false/true
+   */
+  calculateTotalPriceByQuantity?: boolean;
+  onDelete: (id: string) => void;
+  onEdit: (id: string) => void;
+  onClick: (id: string) => Promise<void>;
+}
