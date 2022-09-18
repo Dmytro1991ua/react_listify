f4c00f6f930730d7ff4c116135a93abebcc635c1
Dmytro Kurchenko <61331410+Dmytro1991ua@users.noreply.github.com>
2022-09-18 13:38:16 +0300

    Create basic Profile page layout  (#101)
    * Add missing import related to authService
    
    * Create basic Profile page layout
    
    Co-authored-by: Dmytro Kurchenko <dmytro.kurchenko.-nd@disney.com>

--- client/src/app/modules/auth/auth.actions.ts
+++ client/src/app/modules/auth/auth.actions.ts
@@ -1,13 +1,13 @@
 import { authService } from './auth.service';
 import { useAuthStore } from './auth.store';
 
-export const validateUserAction = async (): Promise<void> => {
+export const validateUserAction = async (firebaseProviders: string[]): Promise<void> => {
   const setUser = useAuthStore.getState().setUser;
   try {
     const validatedUser = await authService.validateUser();
 
     if (validatedUser) {
-      setUser(validatedUser);
+      setUser({ ...validatedUser, firebaseProviders });
     }
   } catch (error) {
     setUser(null);
