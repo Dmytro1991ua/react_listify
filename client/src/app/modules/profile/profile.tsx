7337272e313dfcf32515f1489e16f38f5899fbb2
Dmytro Kurchenko <dmytro.kurchenko.-nd@disney.com>
2022-09-18 13:35:23 +0300

    Create basic Profile page layout

--- client/src/app/modules/profile/profile.tsx
+++ client/src/app/modules/profile/profile.tsx
@@ -1,7 +1,61 @@
 import { ReactElement } from 'react';
 
+import { toastService } from '../../services/toast.service';
+import SectionHeader from '../../shared/components/section-header/section-header';
+import { useAuthStore } from '../auth/auth.store';
+import {
+  BlockTitle,
+  CommonProfileBlock,
+  CommonProfileBlockTitle,
+  ProfileAccountSettingsWrapper,
+} from './profile.styled';
+
 const Profile = (): ReactElement => {
-  return <div>Profile</div>;
+  const user = useAuthStore((state) => state.user);
+  const hasEmailAndPasswordProvider = user?.firebaseProviders?.includes('password');
+
+  const renderUserInfoBlock = (
+    <CommonProfileBlock>
+      <CommonProfileBlockTitle variant='h4'>User Information</CommonProfileBlockTitle>
+    </CommonProfileBlock>
+  );
+
+  const renderChangePasswordBlock = (
+    <CommonProfileBlock>
+      <CommonProfileBlockTitle variant='h4'>Change Password</CommonProfileBlockTitle>
+    </CommonProfileBlock>
+  );
+
+  const renderAccountSettingsBlocks = (
+    <>
+      <BlockTitle variant='h3'>Account Settings</BlockTitle>
+      <ProfileAccountSettingsWrapper hasEmailAndPasswordProvider={hasEmailAndPasswordProvider}>
+        {renderUserInfoBlock}
+        {hasEmailAndPasswordProvider && renderChangePasswordBlock}
+      </ProfileAccountSettingsWrapper>
+    </>
+  );
+
+  const renderApplicationSettingsBlock = (
+    <>
+      <BlockTitle variant='h3'>Application Settings</BlockTitle>
+      <CommonProfileBlock>
+        <CommonProfileBlockTitle variant='h4'>Change Application&apos;s global preferences</CommonProfileBlockTitle>
+      </CommonProfileBlock>
+    </>
+  );
+
+  return (
+    <section>
+      <SectionHeader
+        primaryBtnLabel='Save'
+        title='Profile'
+        onPrimaryButtonClick={() => toastService.info('Not Implemented yet')}
+      />
+      {renderAccountSettingsBlocks}
+      {renderApplicationSettingsBlock}
+    </section>
+  );
 };
 
 export default Profile;
