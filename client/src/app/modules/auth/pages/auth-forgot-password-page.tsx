0fb505faab144397fdc7a261caa5dbc44cb64e70
Dmytro Kurchenko <61331410+Dmytro1991ua@users.noreply.github.com>
2022-10-04 19:48:14 +0300

    Refactor Auth components (#120)
    * Refactor Auth pages

--- client/src/app/modules/auth/pages/auth-forgot-password-page.tsx
+++ client/src/app/modules/auth/pages/auth-forgot-password-page.tsx
@@ -1,27 +1,16 @@
-import { FormikHelpers } from 'formik';
 import { ReactElement } from 'react';
 
 import ForgotPasswordPreviewImage from '../../../../assets/images/auth/forgot-password.jpg';
-import { ForgotPasswordFormInitialValues } from '../auth.interfaces';
-import { authService } from '../auth.service';
 import AuthForgotPasswordForm from '../components/auth-forgot-password-form/auth-forgot-password-form';
 import {
   FORGOT_PASSWORD_FORM_INITIAL_VALUE,
   FORGOT_PASSWORD_FORM_VALIDATION,
 } from '../components/auth-forgot-password-form/auth-forgot-password-form.schema';
+import { useAuth } from '../hooks/useAuth';
 import AuthLayout from './auth-layout/auth-layout';
 
 const AuthForgotPasswordPage = (): ReactElement => {
-  async function handleFormSubmit(
-    values: ForgotPasswordFormInitialValues,
-    actions: FormikHelpers<ForgotPasswordFormInitialValues>
-  ): Promise<void> {
-    const { email } = values;
-
-    await authService.forgotPassword(email);
-
-    actions.resetForm();
-  }
+  const { onForgotPasswordFormSubmit } = useAuth();
 
   return (
     <AuthLayout
@@ -32,7 +21,7 @@ const AuthForgotPasswordPage = (): ReactElement => {
       <AuthForgotPasswordForm
         initialValues={FORGOT_PASSWORD_FORM_INITIAL_VALUE}
         validationSchema={FORGOT_PASSWORD_FORM_VALIDATION}
-        onSubmit={handleFormSubmit}
+        onSubmit={onForgotPasswordFormSubmit}
       />
     </AuthLayout>
   );
